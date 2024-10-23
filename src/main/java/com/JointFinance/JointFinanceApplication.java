92b15140db7dfa7a1fbd92930ddf29ecb7f6eafa
Lilyppu <lily@ppu.co.id>
2024-10-23 12:22:28 +0700

    first commit

---
+++ src/main/java/com/JointFinance/JointFinanceApplication.java
@@ -0,0 +1,13 @@
+package com.JointFinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JointFinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JointFinanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/JointFinance/JointFinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JointFinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JointFinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
