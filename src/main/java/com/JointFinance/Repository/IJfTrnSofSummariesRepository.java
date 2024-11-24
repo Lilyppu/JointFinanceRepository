99a3bb7ff317cba3274fa93f8d598dd2b27e6912
Didik Sudarsono <d1d1_sono@yahoo.com>
2024-11-24 20:16:00 +0700

    didiks

--- src/main/java/com/JointFinance/Repository/IJfTrnSofSummariesRepository.java
+++ src/main/java/com/JointFinance/Repository/IJfTrnSofSummariesRepository.java
@@ -1,6 +1,10 @@
 package com.JointFinance.Repository;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.JointFinance.Models.JfTrnSofSummaries;
@@ -8,5 +12,17 @@ import com.JointFinance.Models.JfTrnSofSummariesCompKey;
 
 @Repository
 public interface IJfTrnSofSummariesRepository extends JpaRepository<JfTrnSofSummaries, JfTrnSofSummariesCompKey> {
-
+	
+	@Query(value = "SELECT jtss.*\r\n"
+				+ "  FROM JF_TRN_SOF_SUMMARIES jtss\r\n"
+				+ " WHERE TRUNC (jtss.SOF_DATE) <= ?1\r\n"
+				+ "       AND jtss.APPROVE_STATUS = 'NA'\r\n"
+				+ "       AND PRNC_AMT > 0\r\n"
+				+ "       AND (EXISTS (SELECT 1 FROM JF_TRN_SOF_HDRS jtsh WHERE jtsh.JF_MODEL = 'JF'\r\n"
+				+ "                    AND jtsh.SOF_CODE = jtss.SOF_CODE) OR jtss.SOF_CODE = '000')\r\n"
+				+ "       AND NOT EXISTS (SELECT 1 FROM JF_TRN_SOF_SUMMARIES jtss2\r\n"
+				+ "                        WHERE APPROVE_STATUS = 'AP'\r\n"
+				+ "                          AND jtss2.SOF_CODE = jtss.SOF_CODE\r\n"
+				+ "                          AND TRUNC (SOF_DATE) = ?1)", nativeQuery = true)
+	public List<JfTrnSofSummaries> getListJfTrnSofSummariesBySofDate(Date sofDate);
 }
